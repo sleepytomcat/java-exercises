9f7e93fe9bbe5c579052ebd5bdd6ef2a1c1521cc
Sergei <sleepytomcat@gmail.com>
2017-05-12 00:22:07 -0700

    Float.NaN, Double.NaN

--- literals/Literals.java
+++ literals/Literals.java
@@ -6,17 +6,19 @@ public class Literals {
 	long d = -1_000_000_000L;
 	float negativeInfinity = -1f/0f; 
 	float e = -123.456f;
-	double f = -1024E45d; 
-	double g = 0xff; // hex numeral
-	double h = 0b0101; // binary numeral
-	double i = 07d; // octal numeral
+	float f = Float.NaN;
+	double g = -1024E45d; 
+	double h = 0xff; // hex numeral
+	double i = 0b0101; // binary numeral
+	double j = 07d; // octal numeral
+	double k = Double.NaN;
 	boolean toBeOrNotToBe = true;
-	char j = 'A';
-	char k = '\\';
-	char l = '\n';
-	char m = '\uFFFF';
-	Object n = null;
-	String o = "Hello";
-	String p = "Hello," + " " + "world!";
+	char l = 'A';
+	char m = '\\';
+	char n = '\n';
+	char o = '\uFFFF';
+	Object p = null;
+	String q = "Hello";
+	String r = "Hello," + " " + "world!";
     }
 }
