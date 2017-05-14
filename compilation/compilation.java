bb2beacf992eb6f80c9d1166773eaf79cb1838f8
Sergei <sleepytomcat@gmail.com>
2017-05-13 18:49:55 -0700

    Compiling a class that references another class.

---
+++ compilation/compilation.java
@@ -0,0 +1,5 @@
+public class compilation {
+    public static void main(String[] args) {
+	System.out.println(otherClass.text());
+    }
+}
---
+++ compilation/otherClass.java
@@ -0,0 +1,3 @@
+class otherClass {
+    public static String text() {return "otherClass.text() returned something.";}
+}
