ade167b2acf794b6f169a5cbc7c974abf5ca1c77
Sergei <sleepytomcat@gmail.com>
2017-05-17 23:27:27 -0700

    Thread interruption

---
+++ interrupted-thread/interruptedthread.java
@@ -0,0 +1,34 @@
+class InterruptedThread {
+    public static void main(String[] args){
+	MyThread t = new MyThread();
+	t.start();
+	try {
+	    Thread.sleep(5000);
+	}
+	catch (InterruptedException e) {
+	    System.out.println("main interrupted");
+	}
+
+	t.interrupt();
+	try {
+	    t.join();
+	}
+	catch (InterruptedException e) {
+	    System.out.println("main interrupted");
+	}
+    }
+}
+
+class MyThread extends Thread {
+    public void run() {
+	try {
+	    while (true) {
+		System.out.println("thread running");
+		sleep(1000);
+	    }
+	}
+	catch (InterruptedException e) {
+	    System.out.println("thread interrupted");
+	}
+    }
+}
