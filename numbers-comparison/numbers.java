c86b5258008c37c2c2705cde657439bab213b329
Sergei <sleepytomcat@gmail.com>
2017-05-27 19:16:04 -0700

    Numbers comparison by reference (which you should never do)

---
+++ numbers-comparison/numbers.java
@@ -0,0 +1,11 @@
+class NumbersComparison {
+    public static void main(String[] args) {
+	Integer x = 127;
+	Integer y = 127;
+	System.out.println(x == y);
+
+	Integer v = 129;
+	Integer w = 129;
+	System.out.println(v == w);
+    }
+}
