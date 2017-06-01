0cbe220fca7090810e24861f88d5f9fc10de28c9
Sergei <sleepytomcat@gmail.com>
2017-05-31 23:40:29 -0700

    Pre-defined annotations listed

---
+++ annotations-predefined/AnnotationsPredefined.java
@@ -0,0 +1,17 @@
+import java.lang.Deprecated;
+import java.lang.Override;
+import java.lang.SuppressWarnings;
+import java.lang.SafeVarargs;
+import java.lang.FunctionalInterface;
+
+// applied to other annotations, 'meta-annotations'
+import java.lang.annotation.Retention;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Target;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Repeatable;
+
+public class AnnotationsPredefined {
+    public static void main(String[] args) {
+    }
+}
