8e0625949c55f36b3ef8b0aa828c40238056075c
Sergei <sleepytomcat@gmail.com>
2017-06-18 23:11:18 -0700

    Reasoning fix

--- generics-basics/GenericsBasics.java
+++ generics-basics/GenericsBasics.java
@@ -5,7 +5,7 @@ public class GenericsBasics {
 	System.out.println(checkedObj);
 
 	Pair<Long, String> obj = new Pair<>(3L, "hello"); // types can be inferred by the compiler
-	Pair<String, Long> otherObj = new Pair<>();
+	Pair<String, Long> otherObj = new Pair<>(); // 'diamond' can be used with generic classes only
 
 	// Also can use "raw type" - class without type arguments provided;
 	// raw types exist for compatibility reasons (Java had no generics until Java 5).
@@ -14,6 +14,15 @@ public class GenericsBasics {
 
 	// The other way round, obviously, Ok:
 	Pair rawObj = new Pair<String, String>();
+	
+	// 'Regular' class with generic constructor argument
+	NonGenericClass ngc = new <Integer>NonGenericClass(128); // type argument explicitly declared
+	NonGenericClass ngc2 = new NonGenericClass("hello"); // type argument inferred
+
+	// 'regular' class with generic method(s) 
+	
+	String text = ngc.foo(1024L); // type argument inferred
+	String text2 = ngc.<String>foo("hello"); // type argument explicitly declared
     }
 }
 
@@ -50,14 +59,20 @@ class GenericComparator<T> implements Comparable<T> {
 	return (lhs.toString().compareTo(rhs.toString()) == -1);    
     }
     
-    // static method cannot use class' type argument, as class type arguments are associated with class instances;
-    // however, static methods can be generic with its own type argument
+    // Static method cannot use class' type argument, as in reality generic type arguments do not really
+    // generate new classes - instead, with type erasure, same raw type used with every generic class
+    // (which means all 'specialized classes' of a generic class actually share same static data/methods.
+    // However, static methods can be generic with its own type argument(s).
     static <U> void foo(U data) {
 	System.out.println(data.toString());
     }
 }
 
 class NonGenericClass {
+    <U> NonGenericClass(U data) { // constructors can be generic, too
+	System.out.println(data.toString());
+    }
+
     <T> String foo(T data) { // non-generic class still can have generic methods
 	return "foo:" + data.toString(); 
     }
@@ -66,5 +81,3 @@ class NonGenericClass {
 	return "bar:" + data.toString();
     }
 }
-
-
