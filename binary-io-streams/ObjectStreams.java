cd12fe8bd04435534e6e77ed2a12599baee513c3
Sergei <sleepytomcat@gmail.com>
2017-07-21 23:38:53 -0700

    Object stream example updated

--- binary-io-streams/ObjectStreams.java
+++ binary-io-streams/ObjectStreams.java
@@ -4,6 +4,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ObjectOutputStream;
 import java.io.ObjectInputStream;
+import java.io.Serializable;
+import java.io.EOFException;
 import java.util.Arrays;
 
 class ObjectStreams {
@@ -17,27 +19,60 @@ class ObjectStreams {
 	try (	ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
 		ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteOutputStream)) {
 		
-		objectOutputStream.writeObject(new String("Hello, world!")); // String object
+		MyClass obj = new MyClass();
+		obj.x = 11;
+		obj.y = 22;
+		obj.z = "Hello, world!";
+		obj.w.a = 33;
+		obj.w.b = "Hello again!";
+
+		objectOutputStream.writeObject(obj);
+
+		// some more objects
+		objectOutputStream.writeObject(new String("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+		objectOutputStream.writeObject(new Boolean(true));
+		objectOutputStream.writeObject(new Byte((byte)42));
 		data = byteOutputStream.toByteArray();
+		System.out.println("Written " + byteOutputStream.toByteArray().length + " bytes");
+		System.out.println("Data: " + Arrays.toString(byteOutputStream.toByteArray()));
 	}
 	catch (Exception ex) {
 	    ex.printStackTrace();
 	}
 	System.out.println();
 
-	System.out.println("Reading objects from object streame:");
+	System.out.println("Reading objects from object stream:");
 	try (	InputStream byteInputStream = new ByteArrayInputStream(data);
 		ObjectInputStream objectInputStream = new ObjectInputStream(byteInputStream)) {
-	    Object x = objectInputStream.readObject();
-	    System.out.println(x);
-	x = objectInputStream.readObject();
-	    System.out.println(x);
-x = objectInputStream.readObject();
-	    System.out.println(x);
-}
+	    while (true) {
+		Object obj = objectInputStream.readObject();
+		System.out.println(obj.getClass() + ", " + obj);
+	    }
+	}
+	catch (EOFException ex) {
+	    System.out.println("End of stream");
+	}
 	catch (Exception ex) {
 	    ex.printStackTrace();
 	}
 	System.out.println();
     }
+}
+
+// must implement Serializable
+class MyClass implements Serializable {
+    @Override
+    public String toString() {return "{x:" + x + ", y:" + y +  ", z:\"" + z + "\"" + ", w:" + w + "}";}
+    public int x;
+    public transient Integer y; // will be ignored by serialization
+    public String z;
+    public TestData w = new TestData();
+}
+
+// must be Serializable, too
+class TestData implements Serializable {
+    @Override
+    public String toString() {return "{a:" + a + ", b:\"" + b + "\"}";}
+    public int a;
+    public String b;
 }
