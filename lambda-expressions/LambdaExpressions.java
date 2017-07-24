e5d3a6d24ab4fc805153b8dba43e8730f882eeaa
Sergei <sleepytomcat@gmail.com>
2017-07-23 23:54:17 -0700

    Lambda expressions assorted forms

--- lambda-expressions/LambdaExpressions.java
+++ lambda-expressions/LambdaExpressions.java
@@ -10,19 +10,54 @@ public class LambdaExpressions {
 	});
 
 	// assigning lambda expression to a functor
-	Func functor = str -> System.out.println("Lambda expression assigned to variable, then applied to '" 
-						+ str + "' and '" + localData + "'");
+	Func1arg functor = str -> System.out.println("Lambda expression assigned to variable, then applied to '" 
+							+ str + "' and '" + localData + "'");
 	obj.apply(functor);
     
 	// using instance methods as lambda
 	System.out.println("Passing object method in place of lambda:");
-	obj.apply(System.out::println);
+	obj.apply((Func1arg)(System.out::println));
+    
+	// lambda expression with several arguments
+	obj.apply((a, b, c) -> System.out.println("Lambda with multiple arguments:'" + a + "+" + b + "+" + c + "'"));
+
+	// also, lambda expression can have zero arguments
+	obj.apply(()->{System.out.println("Lambda may have zero arguments");});
+
+	// lambda expressions may be assigned to variables
+	Func0args zeroArgs = ()->{};
+	Func1arg oneArg = str->{};
+	Func3args threeArgs = (x,y,z)->{};
+	obj.apply(zeroArgs);
+	obj.apply(oneArg);
+	obj.apply(threeArgs);
+
+	obj.doAdd((a,b) -> a+b); // simplified form for lambda expressions returning a value
+	obj.doAdd((a,b) -> {return a + b;}); // 'regular' form
     }
 
-    void apply(Func f) {f.process("lambda-argument");}
+    void apply(Func1arg f) {f.process("lambda-argument");}
+    void apply(Func0args f) {f.process();}
+    void apply(Func3args f) {f.process("prefix", "text", "postfix");}
+    void doAdd(Summation f) {System.out.println("1 + 2 = " + f.sum(1,2));}
 }
 
 @FunctionalInterface // interface should provide exactly one abstract method (default methods do not count)
-interface Func {
+interface Func1arg {
     void process(String textToProcess);
+}
+
+@FunctionalInterface
+interface Func0args {
+    void process();
+}
+
+@FunctionalInterface 
+interface Func3args {
+    void process(String prefix, String text, String suffic);
+}
+
+@FunctionalInterface
+interface Summation {
+    int sum(int a, int b);
 }
