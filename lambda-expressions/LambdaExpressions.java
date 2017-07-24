f09a238c1baaab8746dea6ae50fd21af27aaf2b2
Sergei <sleepytomcat@gmail.com>
2017-07-23 22:32:11 -0700

    Lambda expressions updated

--- lambda-expressions/LambdaExpressions.java
+++ lambda-expressions/LambdaExpressions.java
@@ -2,11 +2,16 @@ public class LambdaExpressions {
     public static void main(String[] args) {
 	String localData = "local-data"; // should be final or effectively final to be accessible within lambda, below
 	LambdaExpressions obj = new LambdaExpressions();
-
+	
+	// using lambda expression in-place
 	obj.apply(text -> {
-		System.out.println("Lambda expression can access received '" + text + "'");
-		System.out.println("Labmda expression can also access local variables, i.e. '" + localData + "'");
+		System.out.println("In-place lambda expression can access received '" + text + "'");
+		System.out.println("In-place lambda expression can also access local variables, i.e. '" + localData + "'");
 	});
+    
+	Func functor = str -> System.out.println("Lambda expression assigned to variable, then applied to '" 
+						+ str + "' and '" + localData + "'");
+	obj.apply(functor);
     }
 
     void apply(Func f) {f.process("lambda-argument");}
