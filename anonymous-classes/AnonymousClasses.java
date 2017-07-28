215e13fffd644d55cab04fdc8865c7bd001491c6
Sergei <sleepytomcat@gmail.com>
2017-07-28 00:30:37 -0700

    Instance initializer block and arguments for anonymous class construction added

--- anonymous-classes/AnonymousClasses.java
+++ anonymous-classes/AnonymousClasses.java
@@ -8,13 +8,19 @@ class AnonymousClasses {
 	    public void g() {System.out.println("Anonymous class implements I.g()");}
 	    // can add new methods, too
 	    public void x() {System.out.println("New method x()");};
+	    // can have 'instance initializer block' which is 
+	    // mostly same as static initializer block - but not static:
+	    {
+		System.out.println("Anonymous classes cannot have a 'real' constructors, so they can use initializer blocks");
+	    }
 	};
 
 	obj.f();
 	obj.g();
 
 	String localString = "some text";
-	MyClass obj2 = new MyClass() {
+	// Note: base class for anonymous class still can get a constructor argument(s)
+	MyClass obj2 = new MyClass(123) {
 	    @Override
 	    void h() {
 		super.h();
@@ -35,5 +41,6 @@ interface I {
 }
 
 class MyClass {
+    MyClass(int arg) {System.out.println("MyClass instance constructed with argument " + arg);}
     void h() {System.out.println("MyClass.h() called");}
 }
