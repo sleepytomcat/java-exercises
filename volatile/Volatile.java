31c37c1e27308e5bf56692d8a8b041e59595b78e
Sergei <sleepytomcat@gmail.com>
2017-07-30 01:02:11 -0700

    Some comments and clarifications of 'happens-before'.

--- volatile/Volatile.java
+++ volatile/Volatile.java
@@ -4,9 +4,12 @@ public class Volatile {
     // but will be properly updated from main memory every time.
     //
     // In this example ChangeListener runs a loop heavily, so the chances are high JVM (in absence of 'volatile')
-    // would decide to cache MY_INT value in local thread memory, which would effectively mean
-    // ChangeListener and ChangeMaker are writing into different variables (however, it's not so clear from the code).
-    private static volatile int MY_INT = 0;
+    // would decide to cache MY_INT value in thread's local memory, which would effectively mean
+    // ChangeListener and ChangeMaker are writing into two different variables (however, it's not so clear from the code).
+    //
+    // Other solution to the problem could be synchronised access to data, as synchronizaton also creates 'happens-before'
+    // relation in Java memory model.
+    private static /* volatile */ int MY_INT = 0;
     public static void main(String[] args) {
 	new ChangeListener().start();
 	new ChangeMaker().start();
@@ -23,7 +26,8 @@ public class Volatile {
 		}
 
 		// Adding the Thread.sleep(500) would likely 'fix' the issue even if there's no 'volatile' modifier
-		// applied to MY_INT
+		// applied to MY_INT - the reason for this JVM would decide it's Ok to propagate shared memory changes to 
+		// local thread memory.
 		// try {Thread.sleep(500);} catch (InterruptedException e) {e.printStackTrace();}
 	    }
 	}
@@ -33,7 +37,7 @@ public class Volatile {
 	@Override
 	public void run() {
 	    int local_value = MY_INT;
-	    while (MY_INT < 5) {
+	    while (local_value < 5) {
 		System.out.println("Incrementing MY_INT to " + (local_value + 1));
 		MY_INT = ++local_value;
 		try {
