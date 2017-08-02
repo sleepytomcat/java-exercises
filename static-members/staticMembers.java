61dac4ca7ef941bcf0eb27e357f3d66aef642e45
Sergei <sleepytomcat@gmail.com>
2017-08-01 23:35:37 -0700

    static initialization updated

--- static-members/staticMembers.java
+++ static-members/staticMembers.java
@@ -1,13 +1,27 @@
 public class staticMembers {
-    static int x = 10;
-    static String y = "hello";
-    static {
+    static int x = f(5); // can use method call to initialize static members
+    static int y = x + 1; // can even reference other static fields; 
+    static String text = "hello";
+    static String word;
+    static int f(int m) {return m*2;}
+    
+    static { // 'static constructor'
 	System.out.println("Static block of code executed");
+	word = "hello, world";
     }
 
     public static void main(String args[]) {
 	System.out.println("main() called");
+
+	System.out.println("...about to reference MyClass for the first time");
+	int h = MyClass.g; // MyClass static constructor(s) will be called just before this statement
     }
 }
 
+class MyClass {
+    static {
+	System.out.println("MyClass static constructor called");
+    }
 
+    static int g = 0;
+}
