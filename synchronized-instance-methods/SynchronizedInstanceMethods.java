041d55c413f56b958583eb679b2741642ec1e47c
Sergei <sleepytomcat@gmail.com>
2017-08-04 13:25:03 -0700

    Synchronized instance methods

---
+++ synchronized-instance-methods/SynchronizedInstanceMethods.java
@@ -0,0 +1,81 @@
+public class SynchronizedInstanceMethods {
+    public static void main(String[] args) throws InterruptedException {
+	SynchronizedStorage storage = new SynchronizedStorage();
+
+	Thread threadX = new Thread(new Producer(storage));
+	Thread threadY = new Thread(new Consumer(storage));
+
+	threadX.start();
+	threadY.start();
+	
+	threadX.join();
+	threadY.join();
+    }
+}
+
+class Consumer implements Runnable {
+    Consumer(SynchronizedStorage storage) {
+	this.storage = storage;
+    }
+
+    @Override
+    public void run() {
+	try {
+	    for (int i = 0; i < 10; ++i) {
+		System.out.println("Consumer about to get value");
+		Integer value = null;
+		while((value = storage.get()) == null);
+		System.out.println("Consumer got " + value);
+	    }
+	}
+	catch (Exception e) {
+	    e.printStackTrace();
+	}
+    }
+    
+    private SynchronizedStorage storage;
+}
+
+class Producer implements Runnable {
+    Producer(SynchronizedStorage storage) {
+	this.storage = storage;
+    }
+
+    @Override
+    public void run() {
+	try {
+	    for (int i = 0; i < 10; ++i) {
+		System.out.println("Producer about to put value " + i);
+		while (!storage.put(i));
+		Thread.sleep(500);
+	    }
+	}
+	catch (InterruptedException e) {
+	    e.printStackTrace();
+	}
+    }
+    
+    private SynchronizedStorage storage;
+}
+
+class SynchronizedStorage {
+    // Class instance is used as a lock for all synchronized instance methods within the class,
+    // so put() and get() could be entered by no more than one thread at the same time.
+    synchronized Integer get() {
+        Integer result = data;
+        data = null;
+        return result;
+    }
+    
+    synchronized boolean put(Integer value) {
+	if (data == null) {
+	    data = value;
+	    return true;
+	}
+	else {
+	    return false;
+	}
+    }
+
+    Integer data = null;
+}
