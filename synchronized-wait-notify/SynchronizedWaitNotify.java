43c7295fa551e8a90f6406cea1861688bc345c3c
Sergei <sleepytomcat@gmail.com>
2017-08-17 09:57:39 -0700

    synchronized producer-consumer example, with wait() and   notify()

---
+++ synchronized-wait-notify/SynchronizedWaitNotify.java
@@ -0,0 +1,93 @@
+import java.util.List;
+import java.util.LinkedList;
+
+public class SynchronizedWaitNotify {
+    public static void main(String[] args) throws InterruptedException {
+	DataQueue queue = new DataQueue(5);
+	Thread producerThread = new Thread(new Producer(queue));
+	Thread consumerThread = new Thread(new Consumer(queue));
+
+	consumerThread.start();
+	producerThread.start();
+
+	consumerThread.join();
+	producerThread.join();
+    }
+}
+
+class Producer implements Runnable {
+    Producer(DataQueue queue) {
+	this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+	while (true)
+	    try {
+		int value = nextValue++;
+		Thread.sleep(10);
+		queue.put(value);
+		System.out.println("produced value " + value);
+	    }
+	    catch (InterruptedException e) {
+		e.printStackTrace();
+	    }
+    }
+
+    DataQueue queue;
+    int nextValue = 0;
+}
+
+class Consumer implements Runnable {
+    Consumer(DataQueue queue) {this.queue = queue;}
+
+    @Override
+    public void run() {
+	while (true)
+	    try {
+		Thread.sleep(1000);
+		int value = queue.get();
+		System.out.println("consumed value " + value);
+	    }
+	    catch (InterruptedException e) {
+		e.printStackTrace();
+	    }
+    }
+
+    DataQueue queue;
+}
+
+class DataQueue {
+    DataQueue(int bufferSize) {
+	data = new LinkedList<Integer>();
+	lock = new Object();
+	this.bufferSize = bufferSize;
+    }
+
+    void put(int value) throws InterruptedException {
+	synchronized(lock) {
+	    while (data.size() >= bufferSize) 
+		lock.wait();
+
+	    data.add(value);
+	    System.out.println("added " + value + ", now " + data);
+	    lock.notify();
+	}
+    }
+
+    int get() throws InterruptedException {
+	synchronized(lock) {
+	    while (data.size() == 0) 
+		lock.wait();
+
+	    Integer value = data.remove(0);
+	    System.out.println("removed " + value + ", now " + data);
+	    lock.notify();
+	    return value;
+	}
+    }
+
+    private List<Integer> data;
+    Object lock;
+    int bufferSize;
+}
